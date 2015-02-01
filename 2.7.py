c8c6af564b5d3ef02a27e28ebd1c7c0ea8d79915
krishnapvs <vpuvvad@ncsu.edu>
2015-01-31 19:48:30 -0500

    Added a function which uses stack

--- 2.7.py
+++ 2.7.py
@@ -3,13 +3,27 @@ def palindromeusingStack(head):
 # Function to check if the linked list is a palindrome
 # Key challenges are to identify the size, if it is not avaliable then we run one pointer till the end other till half
 	end=half=head
+	counter = 0
+	stack=[]
 	while(end):
 		end=end.next
+		counter+=1
 		if(end==None):
 			break
+		stack.append(half.cargo)
 		end=end.next
 		half=half.next
-	print half, end
+		counter+=1
+	if(counter%2):
+		half=half.next
+	while(half):
+		if (int(half.cargo)!=int(stack.pop())):
+			print "Not a palindrome"
+			return
+		half=half.next
+	print "Is a palindrome"
+		
+
 
 def palindrome(head):
 # Function to check if linked list is a palindrome
@@ -43,15 +57,15 @@ def printList(l):
 l1=Node(1)
 runner=l1
 
-for i in range(0,10):
+for i in range(0,15):
 	runner.next=Node(i)
 	runner=runner.next
 
-for i in range(9,-1,-1):
+for i in range(11,-1,-1):
 	runner.next=Node(i)
 	runner=runner.next
-
-printList(l1.next)
+l1=l1.next
+#printList(l1)
 
 print 
 palindromeusingStack(l1)
